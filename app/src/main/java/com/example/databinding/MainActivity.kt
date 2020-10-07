45a11b811eb6e415d2ba86c272c644dcb29e016e
Syed Waqar Uddin <49626225+Waqaruddin@users.noreply.github.com>
2020-10-07 11:26:13 -0400

    Data binding demo

---
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mBinding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        var user = User("Mark", "m@gmail.com")
+        mBinding.userInfo = user
+    }
+}
